6954d12e6a13ab21c920a6527c71c2ce81c4bae5
Leonardo <leonardo.hrjesus@hotmail.com>
2019-04-10 22:02:28 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/leonardo/cursomc/domain/Categoria.java
+++ src/main/java/com/leonardo/cursomc/domain/Categoria.java
@@ -1,11 +1,16 @@
 package com.leonardo.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Entity
@@ -16,6 +21,11 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	
+	@JsonManagedReference
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 	}
@@ -49,6 +59,13 @@ public class Categoria implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -66,6 +83,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	 
 
