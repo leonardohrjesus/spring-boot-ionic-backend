1f19ebb6dd8ca186ae747bedf8159007a4f6ae32
Leonardo <leonardo.hrjesus@hotmail.com>
2019-04-11 21:18:48 -0300

    Estado e cidade

---
+++ src/main/java/com/leonardo/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,14 @@
+package com.leonardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonardo.cursomc.domain.Estado;
+
+
+@Repository
+public interface EstadoRepository  extends JpaRepository<Estado, Integer>{
+	
+	
+
+}
