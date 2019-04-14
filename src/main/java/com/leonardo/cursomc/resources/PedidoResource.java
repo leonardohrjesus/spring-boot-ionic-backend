59403286fb77e728a9ec1f50e46a3f51245f2e17
Leonardo <leonardo.hrjesus@hotmail.com>
2019-04-14 14:35:58 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/leonardo/cursomc/resources/PedidoResource.java
+++ src/main/java/com/leonardo/cursomc/resources/PedidoResource.java
@@ -1,11 +1,7 @@
 package com.leonardo.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +18,7 @@ public class PedidoResource {
 	private PedidoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
