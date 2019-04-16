fe85f455c16a9bcdad427f638edbe23f62c81494
Leonardo <leonardo.hrjesus@hotmail.com>
2019-04-16 18:41:06 -0300

    validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/leonardo/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/leonardo/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,10 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.leonardo.cursomc.domain.Cliente;
+import com.leonardo.cursomc.services.validation.ClienteUpdate;
 
+
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/leonardo/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/leonardo/cursomc/services/validation/ClienteUpdateValidator.java
@@ -2,45 +2,47 @@ package com.leonardo.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.leonardo.cursomc.domain.Cliente;
-import com.leonardo.cursomc.domain.enums.TipoCliente;
-import com.leonardo.cursomc.dto.ClienteNewDTO;
+import com.leonardo.cursomc.dto.ClienteDTO;
 import com.leonardo.cursomc.repositories.ClienteRepository;
 import com.leonardo.cursomc.resources.exception.FieldMessage;
-import com.leonardo.cursomc.services.validation.utils.BR;
 
-public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO>  {
+	
+	
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private ClienteRepository repo;
 
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 		
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 
+		@SuppressWarnings("unchecked")
+		Map<String,String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt( map.get("id"));
+		
 		List<FieldMessage> list = new ArrayList<>();
 		
-		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));		
-		}
-		
-
-		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));		
-		}
 		
 		Cliente aux = repo.findByEmail(objDto.getEmail());
-		if (aux != null) {
+		if (aux != null && !aux.getId().equals(uriId)) {
 			list.add(new FieldMessage("email", "Email já existente"));
 			
 		}
