a3348149cdf1961838563cd3041655e51f91b791
Leonardo <leonardo.hrjesus@hotmail.com>
2019-04-22 23:33:12 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/leonardo/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.leonardo.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.leonardo.cursomc.services.DBService;
+
+
+@Configuration
+@Profile("teste")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instatiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.intantiateTestDatabase();
+		return true; 
+	}
+
+}
