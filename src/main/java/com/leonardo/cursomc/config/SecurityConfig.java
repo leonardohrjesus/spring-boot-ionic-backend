e8379891e29a14572040336fe73ea9bb84f864bc
Leonardo <leonardo.hrjesus@hotmail.com>
2019-05-02 21:18:14 -0300

    Implementando autorização

--- src/main/java/com/leonardo/cursomc/config/SecurityConfig.java
+++ src/main/java/com/leonardo/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.leonardo.cursomc.security.JWTAuthenticationFilter;
+import com.leonardo.cursomc.security.JWTAuthorizationFilter;
 import com.leonardo.cursomc.security.JWTUtil;
 
 @Configuration
@@ -62,6 +63,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 		antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
--- src/main/java/com/leonardo/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/leonardo/cursomc/security/JWTAuthenticationFilter.java
@@ -69,7 +69,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 		@Override
         public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
-                throws IOException, ServletException {
+                throws IOException, ServletException { 
 
             response.setStatus(401);
             response.setContentType("application/json"); 
